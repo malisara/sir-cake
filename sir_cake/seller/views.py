100d43b51e3dff98f8b591fafbc2a00fe5e8b3a2
Sara Mali <mali.sara7@gmail.com>
2022-09-29 11:07:17 +0200

    edit item view & template

--- sir_cake/seller/views.py
+++ sir_cake/seller/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from .forms import NewItemForm
 from .models import Item
@@ -14,7 +14,7 @@ def new_item(request):
         if form.is_valid():
             form.save()
             messages.success(request, "New item successfully saved")
-            # TODO redirect
+            return redirect('all_items')
         else:
             messages.error(
                 "Error: Unable to save the item. Please try again.")
@@ -35,3 +35,28 @@ def item_detail(request, pk):
 def all_items(request):
     context = all_products_context(request)
     return render(request, 'seller/all_items.html', context)
+
+
+def edit_item(request, pk):
+    try:
+        item = Item.objects.get(id=pk)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound()
+
+    if request.method == "POST":
+        form = NewItemForm(request.POST, request.FILES, instance=item)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Item is successfully updated')
+            return redirect('all_items')
+        else:
+            messages.error(
+                request, 'Error: item was not updated. Please try again')
+    else:
+        form = NewItemForm(instance=item)
+
+    context = {
+        'item': item,
+        'form': form}
+
+    return render(request, 'seller/edit-item.html', context)
