31a4fde4318aa02f1136426190e834351af7aa6f
Sara Mali <mali.sara7@gmail.com>
2022-12-20 11:34:22 +0100

    decorator for checking if a user is seller

---
+++ sir_cake/seller/decorators.py
@@ -0,0 +1,11 @@
+from django.core.exceptions import PermissionDenied
+from functools import wraps
+
+
+def user_is_seller(function):
+    @wraps(function)
+    def wrap(request, *args, **kwargs):
+        if request.user.is_staff:
+            return function(request, *args, **kwargs)
+        raise PermissionDenied()
+    return wrap
